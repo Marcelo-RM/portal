925ccdfcc031554e98547d09cf7451269bee49a3
Marcelo-RM <marcelordrgs98@gmail.com>
2019-12-05 12:41:56 -0200

    visual tile completo

--- js/master.js
+++ js/master.js
@@ -21,6 +21,45 @@ function createTile(app){
     var tile = document.createElement("div");
     tile.className = "tile";
 
+    //CABECALHO
+    var tileTop = document.createElement("div");
+    tileTop.className = "tileTop";
+
+    //TITULO
+    var title = document.createElement("div");
+    title.className = "tileTitle";
+    title.innerText = app.header;
+
+    //SUBTITULO
+    var subTitle = document.createElement("div");
+    subTitle.className = "tileSubTitle";
+    subTitle.innerText = app.subHeader;
+
+    //CONTEUDO
+    var tileContent = document.createElement("div");
+    tileContent.className = "tileContent";
+
+    //ICONE
+    var icon = document.createElement("i");
+    icon.className = app.icon + " tileImage";
+
+    //RODAPE
+    var tileFooter = document.createElement("div");
+    tileFooter.className = "tileFooter";
+    tileFooter.innerText = app.type;
+
+    // ADICIONANDO ITENS AO CABECALHO DA TILE
+    tileTop.appendChild(title);
+    tileTop.appendChild(subTitle);
+
+    // ADICIONANDO ITENS AO CENTEUDO DA TILE
+    tileContent.appendChild(icon);
+
+    // ADICIONANDO TODOS OS ITENS À TILE
+    tile.appendChild(tileTop);
+    tile.appendChild(tileContent);
+    tile.appendChild(tileFooter);
+
     //TODO: Criar container para tiles.
     var body = document.getElementsByTagName("body")[0];
     body.appendChild(tile);
